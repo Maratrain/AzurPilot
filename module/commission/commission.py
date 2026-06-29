c4ff3cf53fdea19a0f09f5221b0c334b29281126
Msrtria <a13641469213@163.com>
2026-06-29 22:21:36 +0800

    删去WebUI出现的空行，防止Windows 通知截断

--- module/commission/commission.py
+++ module/commission/commission.py
@@ -642,6 +642,7 @@ class RewardCommission(UI, InfoHandler):
                         tracked.append(text)
                     if tracked:
                         msg = '\n'.join(tracked) 
+                        webui_msg = msg.replace('\n\n', '\n')
                         if gem_count >= 50:
                             title = f"AzurPilot <{instance}> 大成功！！！委托获得顶级奖励喵！"
                             webui_title = "大成功！！！委托获得顶级奖励喵！"
@@ -663,7 +664,7 @@ class RewardCommission(UI, InfoHandler):
                         notify_webui(
                             instance,
                             title=webui_title,
-                            content=msg,
+                            content=webui_msg,
                         )
             else:
                 logger.info('Commission income: no known items recognized from all screenshots')
